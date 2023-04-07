83d4db7b4772f1d7a0beb6e1e48d1b912fec8e17
arturmx70 <arturmx70@gmail.com>
2023-04-07 11:31:12 +0200

    some improvements

--- src/components/Middle/index.js
+++ src/components/Middle/index.js
@@ -4,34 +4,52 @@ import { useState, useEffect } from 'react';
 const Middle = function() {
 
   let [data, setData] = useState([]);
-  let [photo, setPhoto] = useState();
-  let [place, setPlace] = useState();
-  let [title, setTitle] = useState();
-  let [time, setTime] = useState();
-
-
 
 //   8O2f06YEeOpoZ0FP0pr3MbkWoDE3DeX8
 //   CmVVCGlwUVoDbtX59c6f0u7kR0fAfcBI
 //   2CxKOCm170DAig3ccKboNG2NV0vhmPOo
-  let keyApi = 'CmVVCGlwUVoDbtX59c6f0u7kR0fAfcBI'
+  let keyApi = '2CxKOCm170DAig3ccKboNG2NV0vhmPOo'
 
 useEffect (function() {
   fetch(`https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${keyApi}`)
   .then((response) => response.json())
   .then((dataObj) => setData(dataObj.results));},[])
   
+//   let data = [];
+// if (data1.length > 0) {
+//   data = data1
+// }
 
-// photos:
-// let photos = data;
-  // for (let i = 0; data.length )
-  console.log(data[1]);
+let category = '';
+let photo = '';
+let place = '';
+let title = '';
+let description = '';
+  for (let i = 0; i < data.length; i++ ) {
+    category = data[i].des_facet[0];
+    photo = data[i].multimedia[1].url;
+    // place = data[i].geo_facet[0];
+    place = (data[i].subsection).toUpperCase();
+    title = data[i].title;
+    description = data[i].abstract;
+  }
+
+console.log(data);
+
+
+
+  
+  
+
     return (
         <div className="middle">
             <div className="container">
               <div className="wrapper">
-                <h2 className="middle__h2">Popular Topics</h2>
-                <img src=""></img>
+                <h2 className="middle__category">{category}</h2>
+                <img className="middle__photo" src= {photo}></img>
+                <h3 className="middle__place">{place}</h3>
+                <h2 className="middle__title">{title}</h2>
+                <p className="middle__description">{description}</p>
               </div>
             </div>
         </div>
